23e0e0ee85a993b83fd21ad18a083a203e38f1c6
Bart Kuijper <bartkuijper@Barts-Air.zyxel.box>
2021-05-08 11:33:00 +0200

    testing

--- index.js
+++ index.js
@@ -27,8 +27,78 @@ app.post("/auth", jsonParser, function (req, res) {
         res.send({ authentication: false });
     }
 });
+var getNumberOfWeek = function () {
+    var today = new Date();
+    var firstDayOfYear = new Date(today.getFullYear(), 0, 1);
+    var pastDaysOfYear = (today.valueOf() - firstDayOfYear.valueOf()) / 86400000;
+    return Math.ceil((pastDaysOfYear + firstDayOfYear.getDay() + 1) / 7);
+};
+var nrWeek = getNumberOfWeek();
+var tasks = [
+    'Take out the trash',
+    'mop the floors',
+    'clean the surfaces',
+    'clean the shower + toilet',
+    'Take out the bottles + Vacuum',
+    'mop the floors',
+    'Take out the trash',
+    'Take out the bottles + Vacuum',
+    'clean the shower + toilet',
+    'clean the surfaces',
+    'mop the floors',
+    'Take out the bottles + Vacuum',
+    'clean the surfaces',
+    'clean the shower + toilet',
+    'Take out the trash',
+    'Take out the bottles + Vacuum',
+    'Take out the trash',
+    'mop the floors',
+    'clean the shower + toilet',
+    'clean the surfaces',
+    'Take out the bottles + Vacuum',
+    'Take out the trash',
+    'mop the floors',
+    'clean the surfaces',
+    'clean the shower + toilet',
+    'mop the floors',
+    'Take out the bottles + Vacuum',
+    'Take out the trash',
+    'clean the shower + toilet',
+    'clean the surfaces',
+    'Take out the trash',
+    'clean the surfaces',
+    'mop the floors',
+    'clean the shower + toilet',
+    'Take out the bottles + Vacuum',
+    'mop the floors',
+    'clean the surfaces',
+    'Take out the trash',
+    'clean the shower + toilet',
+    'Take out the bottles + Vacuum',
+    'Take out the trash',
+    'mop the floors',
+    'Take out the bottles + Vacuum',
+    'clean the surfaces',
+    'clean the shower + toilet',
+    'Take out the trash',
+    'clean the surfaces',
+    'mop the floors',
+    'Take out the bottles + Vacuum',
+    'clean the shower + toilet',
+    'Take out the trash',
+    'mop the floors',
+    'clean the surfaces',
+    'clean the shower + toilet',
+    'Take out the bottles + Vacuum'
+];
+//const allTasks = [...tasks, tasks, tasks, tasks, tasks, tasks, tasks, tasks,tasks,tasks, tasks]
+//const mergedAllTasks = [].concat.apply([], allTasks);
+//console.log(tasks.filter(e => e !== 'xxxx'))
+// Bart your task of this week is " + mergedAllTasks[result + 2]
 app.get("/", jsonParser, function (req, res) {
-    res.send("welcome");
+    var result = getNumberOfWeek();
+    console.log(result);
+    res.send("\n    <h1>Hi everyone! \uD83D\uDC4B it is week: " + (nrWeek - 2) + "</h1>  \n    <ul>\n      <li>Bart your task of this week is: <b> " + tasks[nrWeek] + " </b></li>\n      <li>Camila your task of this week is: <b> " + tasks[nrWeek + 1] + " </b></li>\n      <li>Camille your task of this week is: <b> " + tasks[nrWeek + 2] + " </b></li>\n      <li>Andrej your task of this week is: <b> " + tasks[nrWeek + 3] + " </b></li>\n      <li>Miguel your task of this week is: <b> " + tasks[nrWeek + 4] + " </b></li>\n    </ul>");
 });
 // start the Express server
 app.listen(PORT, function () {
--- index.ts
+++ index.ts
@@ -33,8 +33,93 @@ app.post( "/auth", jsonParser, ( req : express.Request, res ) => {
     }
 } );
 
+const getNumberOfWeek = (): number => {
+  const today = new Date();
+  const firstDayOfYear = new Date(today.getFullYear(), 0, 1);
+  const pastDaysOfYear = (today.valueOf() - firstDayOfYear.valueOf()) / 86400000;
+  return Math.ceil((pastDaysOfYear + firstDayOfYear.getDay() + 1) / 7);
+}
+
+const nrWeek = getNumberOfWeek()
+
+const tasks = [
+  'Take out the trash',
+  'mop the floors',
+  'clean the surfaces',
+  'clean the shower + toilet',
+  'Take out the bottles + Vacuum',
+  'mop the floors',
+  'Take out the trash',
+  'Take out the bottles + Vacuum',
+  'clean the shower + toilet',
+  'clean the surfaces',
+  'mop the floors',
+  'Take out the bottles + Vacuum',
+  'clean the surfaces',
+  'clean the shower + toilet',
+  'Take out the trash',
+  'Take out the bottles + Vacuum',
+  'Take out the trash',
+  'mop the floors',
+  'clean the shower + toilet',
+  'clean the surfaces',
+  'Take out the bottles + Vacuum',
+  'Take out the trash',
+  'mop the floors',
+  'clean the surfaces',
+  'clean the shower + toilet',
+  'mop the floors',
+  'Take out the bottles + Vacuum',
+  'Take out the trash',
+  'clean the shower + toilet',
+  'clean the surfaces',
+  'Take out the trash',
+  'clean the surfaces',
+  'mop the floors',
+  'clean the shower + toilet',
+  'Take out the bottles + Vacuum',
+  'mop the floors',
+  'clean the surfaces',
+  'Take out the trash',
+  'clean the shower + toilet',
+  'Take out the bottles + Vacuum',
+  'Take out the trash',
+  'mop the floors',
+  'Take out the bottles + Vacuum',
+  'clean the surfaces',
+  'clean the shower + toilet',
+  'Take out the trash',
+  'clean the surfaces',
+  'mop the floors',
+  'Take out the bottles + Vacuum',
+  'clean the shower + toilet',
+  'Take out the trash',
+  'mop the floors',
+  'clean the surfaces',
+  'clean the shower + toilet',
+  'Take out the bottles + Vacuum'
+]
+
+//const allTasks = [...tasks, tasks, tasks, tasks, tasks, tasks, tasks, tasks,tasks,tasks, tasks]
+
+//const mergedAllTasks = [].concat.apply([], allTasks);
+
+//console.log(tasks.filter(e => e !== 'xxxx'))
+
+// Bart your task of this week is " + mergedAllTasks[result + 2]
+
 app.get( "/", jsonParser, ( req : express.Request, res ) => {
-  res.send("welcome")
+  const result = getNumberOfWeek()
+  console.log(result)
+  res.send(`
+    <h1>Hi everyone! 👋 it is week: ${nrWeek -2}</h1>  
+    <ul>
+      <li>Bart your task of this week is: <b> ${tasks[nrWeek]} </b></li>
+      <li>Camila your task of this week is: <b> ${tasks[nrWeek + 1]} </b></li>
+      <li>Camille your task of this week is: <b> ${tasks[nrWeek + 2]} </b></li>
+      <li>Andrej your task of this week is: <b> ${tasks[nrWeek + 3]} </b></li>
+      <li>Miguel your task of this week is: <b> ${tasks[nrWeek + 4]} </b></li>
+    </ul>`)
 } );
 
 // start the Express server
